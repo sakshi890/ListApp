df0d6fe7a28ce5179e6aa4126c260c84c20e0a10
sakshi890 <71330127+sakshi890@users.noreply.github.com>
2020-12-04 14:36:55 +0530

    Create  Activity.java
    Adding Items to the ListView

---
+++ Activity.java
@@ -0,0 +1,47 @@
+﻿﻿﻿import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+android.support.v7.app.AppCompatActivity;
+import  import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+ 
+import java.util.ArrayList;
+ 
+public class FabExampleActivity extends AppCompatActivity {
+ 
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    private ListView myListView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fab_example);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        
+        myListView = (ListView) findViewById(R.id.listView);
+ 
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        myListView.setAdapter(adapter);
+        
+        FloatingActionButton fab = (FloatingActionButton) 
+                            findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", 
+                     Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+.
+.
+}
